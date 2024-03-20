23bccab12b9d0bf812ab8266fd64cb5d80233209
munter <munter@sheridancollege.ca>
2024-03-20 10:08:38 -0400

    Task 1 complete

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharacterControl : MonoBehaviour
 {
-    public TMPro.TextMeshProUGUI currentSelection;
+    public TextMeshProUGUI currentSelection;
+    public static CharacterControl Instance;
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -15,13 +18,19 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        Instance.currentSelection.text = villager.ToString();
+    }
+
+    private void Start()
+    {
+        Instance = this;
     }
 
     private void Update()
     {
-        if(SelectedVillager != null)
-        {
-            currentSelection.text = SelectedVillager.GetType().ToString();
-        }
+        //if(SelectedVillager != null)
+        //{
+        //    currentSelection.text = SelectedVillager.GetType().ToString();
+        //}
     }
 }
